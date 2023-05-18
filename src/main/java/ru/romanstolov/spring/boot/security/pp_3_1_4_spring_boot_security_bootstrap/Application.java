7002e104e9b6ca635f2b84583aeb5e67158a7ae5
RomanStolov <roman_stolov@mail.ru>
2023-05-18 16:45:06 +0300

    Таску сделал. Схожесть с примером на видео полная. Начальную страницу сайта с возможностью регистрации нового пользователя на сайте (реализованную в прошлой задаче) в текущей таске оставил.

---
+++ src/main/java/ru/romanstolov/spring/boot/security/pp_3_1_4_spring_boot_security_bootstrap/Application.java
@@ -0,0 +1,13 @@
+package ru.romanstolov.spring.boot.security.pp_3_1_4_spring_boot_security_bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/romanstolov/spring/boot/security/pp_3_1_4_spring_boot_security_bootstrap/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romanstolov.spring.boot.security.pp_3_1_4_spring_boot_security_bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
